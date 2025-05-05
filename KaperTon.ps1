e87a85bb7035a18bd50d628c1dcda487a6faa6e5
Jonesckevin <57996927+Jonesckevin@users.noreply.github.com>
2025-05-04 20:06:54 -0400

    Update KaperTon.ps1

--- KaperTon.ps1
+++ KaperTon.ps1
@@ -2,50 +2,20 @@
 $kapePath = '.\kape.exe'    # Update this to the actual location of kape.exe
 $TSourceList = @('C:')      # List of source drives
 $TDestinationDrive = 'J:'   # Destination drive letter
-$VHDMountPath = "$TDestinationDrive" # Path to mount the VHD which should be the same as TDestinationDrive
 
 $T = '!BasicCollection,!Triage-Singularity'
 $M = '!EZParser'
 $CustoM = '!!CustoM'
 
-$mflushEnabled = $false       # Set to $true to include --mflush, $false to exclude it
-
 # Assign the module flush option based on $mflushEnabled
+$mflushEnabled = $false       # Set to $true to include --mflush, $false to exclude it
 if ($mflushEnabled) {$mf = "--mflush"} else {$mf = ""}
 
-# Ensure the Hyper-V module is available. If not, skip.
-if (-Not (Get-Module -ListAvailable -Name Hyper-V)) {
-    Write-Host "The Hyper-V module is not installed or enabled. Please enable it in Windows Features. To Automate creating and mount a Virtual Disk (VHDX)" -ForegroundColor Red
-} else {
-    Write-Host "The Hyper-V module is available." -ForegroundColor Green
-    # Check if Hyper-V is enabled
-    $hyperVEnabled = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V | Where-Object { $_.State -eq "Enabled" }
-    if (-Not $hyperVEnabled) {
-        Write-Host "Hyper-V is not enabled on this system. Please enable it using 'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All'." -ForegroundColor Red
-    } else {
-        Write-Host "Hyper-V is enabled." -ForegroundColor Green
-        $CaseNum = Read-Host "Please enter the Case Number [e.g 2025-001]" # Prompt for case number
-
-        # Import the Hyper-V module
-        Import-Module Hyper-V -ErrorAction Stop
-
-        # Define the path for the Virtual Disk
-        $VHDPath = "C:\Users\$env:USERNAME\Desktop\Evidence\$CaseNum.vhdx"
-
-        # Create the VHDX file if it doesn't exist and mount it
-        if (-Not (Test-Path $VHDPath)) {
-            $VHDSize = 100GB                    # Size of the VHD
-            $VHDFormat = "Dynamic"              # Format of the VHD (Dynamic or Fixed)
-            $VHDType = "VHDX"                   # Type of the VHD (VHD or VHDX)
-
-            New-VHD -Path $VHDPath -SizeBytes $VHDSize -Dynamic -Format $VHDFormat -VHDType $VHDType
-            Mount-VHD -Path $VHDPath
-        } else {
-            Write-Host "VHDX file already exists. Mounting the VHD..." -ForegroundColor Yellow
-            Mount-VHD -Path $VHDPath
-        }
-    }
-}
+<#
+## Run Virtual Disk Powershell Script & Import the Virtual Disk Handler script if it exists
+$virtualDiskHandlerPath = ".\VirtualDiskHandler.ps1"; if (Test-Path $virtualDiskHandlerPath) {$VHDMountPath = "$TDestinationDrive"; . $virtualDiskHandlerPath} else {
+    Write-Host "Virtual Disk Handler script not found. Skipping VHD creation." -ForegroundColor Yellow}
+#>
 
 ## Loop through each source drive
 $i = 1 # PE01 to PE99 Counter as per TSourceList counting
@@ -56,6 +26,7 @@ foreach ($TSource in $TSourceList) {
     $MDest = "$TDest\module"              # Module destination path
 
     # Create a list of commands to be executed
+
     $commands = @(
         "$kapePath --tsource `"$TSource`" --tdest `"$TDest`" --target `"$T`"",                # Get all targets
         "$kapePath --msource `"$MSource`" --mdest `"$MDest`" $mf --module `"$M`"",            # Get all modules
@@ -85,4 +56,10 @@ foreach ($TSource in $TSourceList) {
     }
 
     $i++
+}
+
+## For Dev Testing, delete the console logs that are created in the folders
+$logFiles = Get-ChildItem -Path $TDestinationDrive -Recurse -Include *ConsoleLog*, *CopyLog.csv, *SkipLog.csv*
+foreach ($logFile in $logFiles) {
+    Remove-Item $logFile.FullName -Force
 }
